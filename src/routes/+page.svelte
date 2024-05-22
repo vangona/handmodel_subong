78d86b29b5345438e305630b088c29e9bc4fad88
vangona <rlarhksrud12@naver.com>
2024-05-22 20:16:56 +0900

    env: shadcn 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
 <h1 class="cursor-pointer">Welcome to SvelteKit</h1>
 <p class="font-bold underline mt-4">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<Button variant="outline" href="/about">Move to about</Button>
