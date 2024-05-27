5c1efe795b098f743e7dca3ace45efa5140108cb
vangona <rlarhksrud12@naver.com>
2024-05-28 00:35:03 +0900

    chore: gasp, locomotive scroll 동작 확인

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,4 @@
-<h1>About</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1>About</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
