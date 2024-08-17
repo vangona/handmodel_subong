714d7ce934196a9bba19edb4e11500810362676d
vangona <rlarhksrud12@naver.com>
2024-08-18 02:01:34 +0900

    feat: supabase 연결 확인

--- src/lib/api/supabaseClient.ts
+++ src/lib/api/supabaseClient.ts
@@ -3,4 +3,38 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export interface SupabaseTables {
+	test: {
+		Row: {
+			// the data expected from .select()
+			id: number;
+			created_at: string;
+			test: string;
+		};
+		Insert: {
+			// the data to be passed to .insert()
+			test: string;
+		};
+		Update: {
+			// the data to be passed to .update()
+			test: string;
+		};
+	};
+}
+
+export interface SupabaseDatabase {
+	public: {
+		Tables: SupabaseTables;
+	};
+}
+
+export type TestTable = SupabaseTables['test']['Row'];
+
+// error는 실제로 any 타입
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isPostgresError = (error: any) => {
+	console.log(error);
+	return Boolean(error.message);
+};
+
+export const supabase = createClient<SupabaseDatabase>(supabaseUrl, supabaseAnonKey);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import { getTest } from '$lib/api/test/testApis';
 	import Hero from '$lib/components/my-ui/hero/hero.svelte';
 </script>
 
 <Hero />
-<div class="min-h-screen"></div>
+<div class="min-h-screen">
+	{#await getTest()}
+		Loading...
+	{:then data}
+		{#each data as testRow}
+			<div>{testRow.test}</div>
+		{/each}
+	{/await}
+</div>
