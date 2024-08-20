38a703a51f7c38065bca11c16958da9b61bf9a51
vangona <rlarhksrud12@naver.com>
2024-08-21 00:48:03 +0900

    feat: 기본 그리드 레이아웃 구현

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { getTest } from '$lib/api/test/testApis';
 	import Hero from '$lib/components/my-ui/hero/hero.svelte';
+	import handSrc from '$lib/assets/images/hero-hand.png';
 </script>
 
 <Hero />
@@ -8,8 +9,19 @@
 	{#await getTest()}
 		Loading...
 	{:then data}
-		{#each data as testRow}
-			<div>{testRow.test}</div>
-		{/each}
+		<div class="grid grid-cols-2 grid-rows-2 gap-4 p-4">
+			{#each data as testRow}
+				<div class="card image-full aspect-square">
+					<figure class="object-cover">
+						<img src={handSrc} alt="hands" class="w-full object-cover" />
+					</figure>
+					<div class="card-body bg-transparent">
+						<div class="card-title">
+							{testRow.test}
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
 	{/await}
 </div>
