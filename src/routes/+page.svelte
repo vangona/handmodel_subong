b16f8a77111678e150ed6ea00cbcc370a5bec060
vangona <rlarhksrud12@naver.com>
2024-09-16 01:27:52 +0900

    feat: 필터 태그 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,54 @@
 	import { getTest } from '$lib/api/test/testApis';
 	import Hero from '$lib/components/my-ui/hero/hero.svelte';
 	import handSrc from '$lib/assets/images/hero-hand.png';
+	import type { TestTable } from '$lib/api/supabaseClient';
+
+	let categorySet: Set<string> = new Set();
+	let categoryArr: Array<string> = [];
+	let selectedCategory: string = 'all';
+	const fetcehdData: Array<TestTable> = [];
+	let processedData: Array<TestTable> = [];
+
+	async function mountTestFetchData() {
+		const data = await getTest();
+		// category를 맵으로 변환
+		data.forEach((testRow) => {			
+			testRow.category.forEach(item => categorySet.add(item));
+		});
+
+		categoryArr = Array.from(categorySet);
+
+		fetcehdData.push(...data);
+		processedData = fetcehdData;
+	}
+
+	const handleClickCategory = (category: string) => {
+		selectedCategory = category;
+		processedData = category === 'all' ? fetcehdData : fetcehdData.filter(item => item.category.includes(category));
+	}
+
+	mountTestFetchData();
 </script>
 
 <Hero />
 <div class="min-h-screen">
-	{#await getTest()}
+	{#await processedData}
 		Loading...
 	{:then data}
 		<div class="card-container grid grid-cols-1 grid-rows-1 p-14">
+			<h3 >				
+				선택된 카테고리 : {selectedCategory}
+			</h3>
+			<div class="category-filter__wrapper">
+				<button class="category-filter__item badge badge-lg badge-ghost" on:click={() => handleClickCategory('all')}>
+					all
+				</button>
+				{#each categoryArr as category}
+					<button class="category-filter__item badge badge-lg badge-ghost" on:click={() => handleClickCategory(category)}>
+						{category}
+					</button>
+				{/each}
+			</div>
 			{#each data as testRow}
 				<div class="card--main card image-full aspect-square">
 					<figure class="object-cover">
@@ -39,6 +79,25 @@
 </div>
 
 <style lang="scss">
+	.category-filter__wrapper {
+		display: flex;
+		gap: 8px;
+		margin-bottom: 16px;
+	}
+	.category-filter__item {
+		position: relative;
+		transition: transform 0.2s ease-in-out;
+		user-select: none;
+		&:hover {
+			cursor: pointer;
+
+		}
+		&:active {
+			transform: scale(0.95);
+		}
+
+	}
+
 	div.card.card--main {
 		&::before {
 			opacity: 0;
