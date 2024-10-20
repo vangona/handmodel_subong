6bab419053f31e464ae67333400d97639d1a41d7
vangona <rlarhksrud12@naver.com>
2024-10-20 22:53:42 +0900

    fox: 상세페이지 사진 번호 잘리는 버그 수정

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -89,7 +89,7 @@
 					<div class="swiper-pagination"></div>
 					<div class="swiper-button-prev"></div>
 					<div class="swiper-button-next"></div>
-					<div class="absolute bottom-4 right-4 bg-black bg-opacity-50 text-white px-2 py-1 rounded">
+					<div class="absolute bottom-4 right-4 bg-black bg-opacity-50 text-white px-2 py-1 rounded z-10">
 						{currentSlide} / {totalSlides}
 					</div>
 				</div>
