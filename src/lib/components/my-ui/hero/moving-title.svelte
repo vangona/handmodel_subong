50e964f18709ad9b25c54abf4730ad5b17254ca3
vangona <rlarhksrud12@naver.com>
2024-10-23 00:14:12 +0900

    feat: 명조 폰트 수정

--- src/lib/components/my-ui/hero/moving-title.svelte
+++ src/lib/components/my-ui/hero/moving-title.svelte
@@ -3,7 +3,10 @@
 	import { onMount } from 'svelte';
 	import handSrc from '$lib/assets/images/hero-hand.png';
 
+	let isMobile = false;
+
 	onMount(() => {
+		isMobile = window.innerWidth < 768;
 		gsap.to('.up', {
 			y: '-50%',
 			duration: 1.5,
@@ -22,13 +25,17 @@
 
 <div class="moving-container relative w-full h-full flex flex-col justify-between">
 	<div class="relative flex-grow flex flex-col justify-center items-center overflow-hidden">
-		<span class="up absolute top-[35%] left-[10%] z-10 font-serif text-white text-2xl md:text-3xl">손모델</span>
+		{#if isMobile}
+			<span class="up absolute top-[35%] left-[10%] z-10 font-serif text-white text-2xl md:text-3xl">손모델</span>
+		{/if}
 		<div class="w-full h-full flex items-center justify-center">
 			<img src={handSrc} alt="손모델 심수연의 손" class="max-w-full max-h-full object-contain" />
 		</div>
-		<span class="down absolute bottom-[30%] right-[10%] z-10 font-serif font-bold text-white text-3xl md:text-4xl">
-			심 수 연
-		</span>
+		{#if isMobile}
+			<span class="down absolute bottom-[30%] right-[10%] z-10 font-serif font-bold text-white text-3xl md:text-4xl">
+				심 수 연
+			</span>
+		{/if}
 	</div>
 </div>
 
