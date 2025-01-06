9a7dc5e93107c60fb5b1fd8334059ec691c1c8d4
vangona <rlarhksrud12@naver.com>
2025-01-06 22:25:56 +0900

    fix: 관리자 게시물 이미지 비율 1:1로 수정

--- src/routes/admin/posts/edit/[id]/+page.svelte
+++ src/routes/admin/posts/edit/[id]/+page.svelte
@@ -241,7 +241,7 @@
 					class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
 					{#each images as image (image.id)}
 						<div class="relative group" animate:flip={{duration: DND_FLIP_DURATION}}>
-							<img src={image.url} alt="업로드된 이미지" class="w-full h-40 object-cover rounded-lg shadow-md" />
+							<img src={image.url} alt="업로드된 이미지" class="w-full aspect-square object-cover rounded-lg shadow-md" />
 							<div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-lg flex items-center justify-center">
 								<button type="button" on:click={() => handleImageDelete(image)} class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2">
 									<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
