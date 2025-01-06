307bff44efec9d87ccb3078337c28a87075574c6
vangona <rlarhksrud12@naver.com>
2025-01-06 22:27:12 +0900

    fix: 관리자에 잘못들어간 텍스트 수정

--- src/routes/admin/images/+page.svelte
+++ src/routes/admin/images/+page.svelte
@@ -157,7 +157,7 @@
 					<svg class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
 					</svg>
-					<p class="mb-2 text-sm text-gray-500"><span class="font-semibold">클릭하��� 업로드</span> 또는 드래그 앤 드롭</p>
+					<p class="mb-2 text-sm text-gray-500"><span class="font-semibold">클릭하여 업로드</span> 또는 드래그 앤 드롭</p>
 					<p class="text-xs text-gray-500">PNG, JPG (최대 10MB)</p>
 				</div>
 				<input 
