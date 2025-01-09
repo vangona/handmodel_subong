e45a9fe57948b4aabcd3c100872a591a4465d31c
vangona <rlarhksrud12@naver.com>
2025-01-09 23:02:45 +0900

    seo: 사이트맵 수정

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -10,35 +10,33 @@ export const GET: RequestHandler = async () => {
 
     // XML 생성
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://subong.vercel.app/</loc>
-            <changefreq>daily</changefreq>
-            <priority>1.0</priority>
-        </url>
-        ${posts?.map(post => `
-        <url>
-            <loc>https://subong.vercel.app/?post=${post.id}</loc>
-            <lastmod>${new Date(post.created_at).toISOString()}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
-        </url>
-        `).join('')}
-        <url>
-            <loc>https://subong.vercel.app/about</loc>
-            <changefreq>monthly</changefreq>
-            <priority>0.7</priority>
-        </url>
-        <url>
-            <loc>https://subong.vercel.app/contact</loc>
-            <changefreq>monthly</changefreq>
-            <priority>0.7</priority>
-        </url>
-    </urlset>`.trim();
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://subong.vercel.app/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>${posts?.map(post => `
+    <url>
+        <loc>https://subong.vercel.app/?post=${post.id}</loc>
+        <lastmod>${new Date(post.created_at).toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>`).join('')}
+    <url>
+        <loc>https://subong.vercel.app/about</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://subong.vercel.app/contact</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+</urlset>`.trim();
 
     return new Response(xml, {
         headers: {
-            'Content-Type': 'application/xml',
+            'Content-Type': 'text/xml',
             'Cache-Control': 'max-age=0, s-maxage=3600'
         }
     });
