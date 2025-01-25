764ce52ff32075fc8b6b6aa51772dce5c04b4f35
vangona <rlarhksrud12@naver.com>
2025-01-25 23:24:30 +0900

    seo: 누락된 Structured data 추가

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -32,6 +32,19 @@
 		isLoading = false;
 		visible = true;
 	});
+
+	const personSchema = {
+		"@context": "https://schema.org",
+		"@type": "Person",
+		"name": "심수연",
+		"jobTitle": "손모델",
+		"url": "https://subong.vercel.app/about",
+		"image": "https://umpactdflfdhwbufbzzp.supabase.co/storage/v1/object/public/post-images/0.6739137500664765.jpg",
+		"description": "다년 간의 촬영 경험과 다양한 제품을 다뤄본 노하우를 바탕으로 원활한 소통과 만족스러운 결과물을 약속드립니다.",
+		"sameAs": [
+			"https://open.kakao.com/o/sBSr9QCc"
+		]
+	};
 </script>
 
 <svelte:head>
@@ -43,6 +56,9 @@
 	<meta property="og:image" content="https://umpactdflfdhwbufbzzp.supabase.co/storage/v1/object/public/post-images/0.6739137500664765.jpg" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://subong.vercel.app/about" />
+	<script type="application/ld+json">
+		{JSON.stringify(personSchema)}
+	</script>
 </svelte:head>
 
 <div class="flex justify-center items-start pt-8 md:pt-12 min-h-screen w-full bg-offwhite bg-opacity-90 pb-24">
