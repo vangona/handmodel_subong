af5b4bd4926e2cc59a740bdfb5353561c28286dd
vangona <rlarhksrud12@naver.com>
2025-03-04 21:29:20 +0900

    feat: ga 스크립트 추가

--- src/lib/components/common/Analytics.svelte
+++ src/lib/components/common/Analytics.svelte
@@ -1,14 +1,11 @@
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-6BVMFYG00K">
-	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VWEVZG8HHL"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
+		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		gtag('config', 'G-6BVMFYG00K');
+
+		gtag('config', 'G-VWEVZG8HHL');
 	</script>
 </svelte:head>
